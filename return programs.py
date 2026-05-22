ce2d59c93a35efa7849a7f7f343578903cb7e8f9
rama4335 <ramadevimuthyala144@gmail.com>
2026-05-22 11:12:15 +0530

    Added new programs

---
+++ return programs.py
@@ -0,0 +1,43 @@
+# def fun(x,y):
+#     return x+y
+# print(fun(10,20))
+
+
+# def fun(x,y):
+#     print(x,y)
+#     return x+y
+#     print(x+x)
+#     return y+y
+# print(fun(10,20))
+
+# def great(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# print(great(75,77))
+
+
+
+# def fun(*a):
+#     return sum(a)
+# x=fun(1,7,8,6,5,3,2,8)
+# if x%2==0:
+#         print("even:",{x})
+# else:
+#         print("odd:",{x})
+
+
+
+# name=input("enter your name:")
+# s=f"name:{name}"
+# print(s)
+
+
+# name="sri"
+# S=f"name:{name}"
+# print(S)
+
+
+
+
