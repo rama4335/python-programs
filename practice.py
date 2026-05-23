1a277486a81f88bee281e5166eaf03075d421593
rama4335 <ramadevimuthyala144@gmail.com>
2026-05-23 16:28:08 +0530

    Added new programs

---
+++ practice.py
@@ -0,0 +1,88 @@
+# def fun(*a):  #even or odd#
+#     for i in a:
+#         if i%2==0:
+#             print("even")
+#         else:
+#             print("odd")
+# fun(1,2,3,4,5,67,78,5,4)
+
+
+# def fun(*a):   #even nums#
+#     for i in a:
+#         if i%2==0:
+#             print(i)
+# fun(1,23,4,56,7,8)
+
+
+# def fun(*a):   #sum#
+#     s=sum(a)
+#     print(s)
+# fun(5,5)
+#
+# def fun(*a):
+#     print(sum(a))
+# fun(1,2,3,45,6,7)
+
+
+# def fun(*a):
+#     for i in a:
+#         if i%2==0:
+#             print("even")
+#         else:
+#             print("odd")
+# fun(9)
+
+
+# def fun(n):
+#     s=n*n
+#     print(s)
+# fun(25)
+
+
+
+# def fun(x,y,z):
+#         if x>y and x>z:
+#             print(x)
+#         elif y>x and y>z:
+#             print(y)
+#         else:
+#             print(z)
+# fun(10,20,30)
+
+
+# def fun(*a):
+#     max=0
+#     for i in a:
+#         if i>max:
+#             max=i
+#     print(max)
+# fun(50,100,78)
+
+
+# def fun(string):
+#     c=0
+#     for i in string:
+#         if i in "aeiou":
+#             c=c+1
+#     print(c)
+# fun("ramadevi")
+
+
+# def fun(string):
+#     for i in string:
+#         if i in "aeiou":
+#             print(i)
+# fun("ramadevi")
+
+
+# def fun(*n):
+#     for num in n:
+#         if num>1:
+#             for i in range(2,num):
+#                 if num%i==0:
+#                     print("not prime")
+#                 else:
+#                     print("prime")
+# fun(2,3,4,5,7,8,6,9)
+
+
